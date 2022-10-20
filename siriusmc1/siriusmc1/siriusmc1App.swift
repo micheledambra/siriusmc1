0190b4b92585b22c5b59b0975372310b228d5849
micheledambra <micheledambra@users.noreply.github.com>
2022-10-20 12:13:40 +0200

    first commit

---
+++ siriusmc1/siriusmc1/siriusmc1App.swift
@@ -0,0 +1,17 @@
+//
+//  siriusmc1App.swift
+//  siriusmc1
+//
+//  Created by Michele D'Ambra on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct siriusmc1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
