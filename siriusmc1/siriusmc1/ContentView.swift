752776061eaa720a1f9f8c6a222652bb8c46a326
Leila Najar <lnajar22@fed.idserve.net>
2022-10-20 12:42:20 +0200

    commit 02

--- siriusmc1/siriusmc1/ContentView.swift
+++ siriusmc1/siriusmc1/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "heart.fill")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.red)
             Text("pull_2")
         }
         .padding()
