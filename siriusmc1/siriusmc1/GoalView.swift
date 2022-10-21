f74ea999be25a6466f107fde9c986769d788426d
Giuseppe <giodice22@fed.idserve.net>
2022-10-21 12:29:04 +0200

    aaa

---
+++ siriusmc1/siriusmc1/GoalView.swift
@@ -0,0 +1,20 @@
+//
+//  GoalView.swift
+//  siriusmc1
+//
+//  Created by Giuseppe Iodice on 21/10/22.
+//
+
+import SwiftUI
+
+struct GoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GoalView_Previews: PreviewProvider {
+    static var previews: some View {
+        GoalView()
+    }
+}
