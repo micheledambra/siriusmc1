7a9d32f85cf4ee51315e4400d9ea431858edb26e
alexnikxx <alexnikxx@users.noreply.github.com>
2022-10-21 13:04:47 +0200

    created new file

---
+++ siriusmc1/siriusmc1/Views/TasksInGoal.swift
@@ -0,0 +1,20 @@
+//
+//  TasksInGoal.swift
+//  siriusmc1
+//
+//  Created by Aleksandra Nikiforova on 21/10/22.
+//
+
+import SwiftUI
+
+struct TasksInGoal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TasksInGoal_Previews: PreviewProvider {
+    static var previews: some View {
+        TasksInGoal()
+    }
+}
