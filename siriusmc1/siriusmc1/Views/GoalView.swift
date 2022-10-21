707b8b3e2ada52958caca1f094916b58de13004e
micheledambra <micheledambra@users.noreply.github.com>
2022-10-22 00:37:37 +0200

    first test goal card

--- siriusmc1/siriusmc1/Views/GoalView.swift
+++ siriusmc1/siriusmc1/Views/GoalView.swift
@@ -2,7 +2,7 @@
 //  GoalView.swift
 //  siriusmc1
 //
-//  Created by Giuseppe Iodice on 21/10/22.
+//  Created by Michele D'Ambra on 21/10/22.
 //
 
 import SwiftUI
@@ -10,7 +10,56 @@ import SwiftUI
 struct GoalView: View {
     var body: some View {
         
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            
+            
+            ZStack(alignment: .top){
+                
+                RoundedRectangle(cornerRadius: 40)
+                    .foregroundColor(.teal)
+                    .frame(width: 330, height: 560)
+                    .shadow(radius: 30)
+                //.padding()
+                
+                VStack(){
+                    Spacer()
+                        .frame(height:35)
+                    
+                    Text("Goal #1")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                    
+                    Spacer()
+                        .frame(height:35)
+                    
+                    Image("statplaceholder")
+                        .resizable()
+                        .frame(width: 200, height:200)
+                        .mask(Circle())
+                        .shadow(radius: 20)
+                    
+                    Spacer()
+                        .frame(height:35)
+                    
+                    Text("Goal description")
+                    
+                    Spacer()
+                        .frame(height:90)
+                    
+                    HStack {
+                        Button("Button") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                        
+                        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Goals")
+        }
     }
 }
 
