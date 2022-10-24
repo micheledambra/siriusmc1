9aa74e48adf7d2bab47bd47fb628f2c8ed19c559
Leila Najar <lnajar22@fed.idserve.net>
2022-10-24 10:03:51 +0200

    commit01

---
+++ siriusmc1/siriusmc1/Views/Task.swift
@@ -0,0 +1,51 @@
+//
+//  Task.swift
+//  siriusmc1
+//
+//  Created by leila najar on 24/10/22.
+//
+
+import SwiftUI
+
+struct Item: Hashable {
+   let name: String
+   let color: UIColor
+}
+
+import Foundation
+struct Tasks: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    
+}
+
+
+struct TaskView: View {
+    
+    var tasks: [Tasks] = [
+        Tasks (name: "Task N°1"), Tasks (name: "Task N°2"), Tasks (name: "Task N°3")
+        ]
+        
+    var body: some View {
+        VStack {
+            Image(systemName: "list.bullet.circle.fill")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Tasks")
+            
+            List {
+                ForEach(tasks) {
+                    task in Text(task.name)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct Task_Previews:
+    PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
