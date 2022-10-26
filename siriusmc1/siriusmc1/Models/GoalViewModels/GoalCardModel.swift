bbd1c282bddae5bc930538a89b1cb4baaea3b7e8
micheledambra <micheledambra@users.noreply.github.com>
2022-10-26 12:14:42 +0200

    add code

--- siriusmc1/siriusmc1/Models/GoalViewModels/GoalCardModel.swift
+++ siriusmc1/siriusmc1/Models/GoalViewModels/GoalCardModel.swift
@@ -21,6 +21,30 @@ struct GoalCard: Identifiable {
     //methods
 }
 
+/*struct Date: {
+    var day: Int
+    var month: Int
+    var year: Int
+}*/
+
+struct GoalTasks: Identifiable {
+    var id: UUID = UUID()
+    
+    var name: String
+    var description : String
+    //var Day: = Date
+    
+    
+    //methods
+}
+
+/*
+var Tasks1: [GoalTasks] = [
+    GoalTasks(name:" Task n 1", description: "Descrizione task 1", day: ),
+
+]*/
+
+
 var GoalList: [GoalCard] = [
     GoalCard(name: "Goal #1", description: "Goal Description 1", numberOfTasks: 5, numberOfTasksDone: 4, color: .teal),
     GoalCard(name: "Goal #2", description: "Goal Description 2", numberOfTasks: 8, numberOfTasksDone: 6, color: .gray),
