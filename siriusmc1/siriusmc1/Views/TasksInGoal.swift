0a8ddf4d6e94f9eb65b23028d399402b80fc7c10
DanyPtz <113537213+DanyPtz@users.noreply.github.com>
2022-10-26 11:11:20 +0200

    added button in goal task

--- siriusmc1/siriusmc1/Views/TasksInGoal.swift
+++ siriusmc1/siriusmc1/Views/TasksInGoal.swift
@@ -8,22 +8,96 @@
 import SwiftUI
 
 struct TasksInGoal: View {
-    @State var setModal: Bool = false
+    @State var showModal: Bool = false
+
     var body: some View {
-        ZStack{
-            Button("Set Tasks") {
-                setModal.toggle()
+        NavigationView {
+            VStack {
+                Divider()
+                Spacer()
+                List {
+                    Button(action: {}) {
+                        HStack {
+                            Image(systemName: "bookmark.circle.fill")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .opacity(0.7)
+                            Text("Task №1")
+                                .fontWeight(.bold)
+                                .font(.system(size: 18))
+                            Text("Task's description                                  ")
+                                .multilineTextAlignment(.leading)
+                        }
+                        .foregroundColor(.white)
+                        .padding(16)
+                    }
+                    .background(.gray)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .cornerRadius(15)
+                       
+                    Button(action: {}) {
+                        HStack {
+                            Image(systemName: "bookmark.circle.fill")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .opacity(0.7)
+                            Text("Task №2")
+                                .fontWeight(.bold)
+                                .font(.system(size: 18))
+                            Text("Task's description                                    ")
+                                .multilineTextAlignment(.leading)
+                        }
+                        .foregroundColor(.white)
+                        .padding(16)
+                    }
+                    .background(.gray)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .cornerRadius(15)
+                
+                    Button(action: {}) {
+                        HStack {
+                            Image(systemName: "bookmark.circle.fill")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .opacity(0.7)
+                            Text("Task №3")
+                                .fontWeight(.bold)
+                                .font(.system(size: 18))
+                            Text("Task's description                                    ")
+                                .multilineTextAlignment(.leading)
+                        }
+                        .foregroundColor(.white)
+                        .padding(16)
+                    }
+                    .background(.gray)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .cornerRadius(15)
+                  
+            
+                }
+                
             }
-            .sheet(isPresented: $setModal) {
-                SetTasksModal()
-            }
+             
+            .navigationTitle("Goal №1")
+           
+            .toolbar(content: {
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Add") {
+                        showModal.toggle()
+                    }
+                    .sheet(isPresented: $showModal) {
+                        SetTasksModal()
+                    }                }
+            })
 
         }
-    }
     
-    struct TasksInGoal_Previews: PreviewProvider {
-        static var previews: some View {
-            TasksInGoal()
-        }
+    }
+}
+
+struct TasksInGoal_Previews: PreviewProvider {
+    static var previews: some View {
+        TasksInGoal()
     }
 }
